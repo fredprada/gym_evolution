a239c6af9010200595bdc5c1c101163aafbae99b
fredprada <fredprada9@gmail.com>
2023-09-08 16:01:00 -0300

    debugando

--- main.py
+++ main.py
@@ -46,11 +46,12 @@ def quantidade_de_series_diferentes(qtd_series):
             lista_box_series.append(exec(f'series_exercicio_1_num_{item} = st.number_input("Séries ex{item}: ", min_value = 0, max_value = 10)'))
             lista_box_reps.append(exec(f'reps_exercicio_1_num_{item} = st.number_input("Núm Reps ex{item}: ", min_value = 0, max_value = 50)'))
             lista_box_carga.append(exec(f'carga_exercicio_1_num_{item} = st.number_input("Carga ex{item}: ", min_value = 0, max_value = 1000)'))
-        return lista_box_series, lista_box_reps, lista_box_carga
+        return [lista_box_series, lista_box_reps, lista_box_carga]
     else:
-        reps_ex_1 = reps_exercicio_1_num_1_ = st.number_input("Núm Reps ex_1: ", min_value = 0, max_value = 50)
-        carga_ex1 = carga_exercicio_1_num_1_ = st.number_input("Carga ex_1: ", min_value = 0, max_value = 1000)
-        return reps_ex_1, carga_ex1
+        series_exercicio_1_num_1_ = st.number_input("Séries ex_1: ", min_value = 0, max_value = 50)
+        reps_exercicio_1_num_1_ = st.number_input("Núm Reps ex_1: ", min_value = 0, max_value = 50)
+        carga_exercicio_1_num_1_ = st.number_input("Carga ex_1: ", min_value = 0, max_value = 1000)
+        return [series_exercicio_1_num_1_, reps_exercicio_1_num_1_, carga_exercicio_1_num_1_]
 
 
 col1, col2 = st.columns([2, 1])
