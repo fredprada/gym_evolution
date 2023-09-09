f84fe44b918bad21dfe30cc59c9cec05168f42a3
fredprada <fredprada9@gmail.com>
2023-09-08 21:43:26 -0300

    debugando

--- exercicio.py
+++ exercicio.py
@@ -4,21 +4,8 @@ class Exercicio:
 
     def get_exercicio(self):
         if self.treino == '1- PEITO | OMBRO | TRÍCEPS':
-            return (['PEITO | SUPINO RETO',
-                     'PEITO | VOADOR'] +
-                    ['OMBRO | ELEVAÇÃO LATERAL',
-                     'OMBRO | DESENVOLVIMENTO COM HALTER'] +
-                    ['TRÍCEPS | TRÍCEPS CORDA',
-                     'TRÍCEPS | TRÍCEPS MERGULHO'])
+            return ['PEITO | SUPINO RETO','PEITO | VOADOR'] +['OMBRO | ELEVAÇÃO LATERAL','OMBRO | DESENVOLVIMENTO COM HALTER'] +['TRÍCEPS | TRÍCEPS CORDA','TRÍCEPS | TRÍCEPS MERGULHO']
         elif self.treino == '2- COSTAS | BÍCEPS':
-            return (['COSTAS | PUXADA ALTA FRONTAL COM PEGADA ABERTA',
-                     'COSTAS | REMADA BAIXA',
-                     'COSTAS | PULL DOWN'] + 
-                    ['BÍCEPS | ROSCA DIRETA',
-                     'BÍCEPS | ROSCA MARTELO'])
+            return ['COSTAS | PUXADA ALTA FRONTAL COM PEGADA ABERTA','COSTAS | REMADA BAIXA','COSTAS | PULL DOWN'] + ['BÍCEPS | ROSCA DIRETA','BÍCEPS | ROSCA MARTELO']
         elif self.treino == '3- PERNA':
-            return (['PERNA | AGACHAMENTO',
-                    'PERNA | EXTENSORA',
-                    'PERNA | FLEXORA',
-                    'PERNA | PANTURRILHA SENTADO',
-                    'PERNA | LEVANTAMENTO TERRA SUMO'])
+            return ['PERNA | AGACHAMENTO','PERNA | EXTENSORA','PERNA | FLEXORA','PERNA | PANTURRILHA SENTADO','PERNA | LEVANTAMENTO TERRA SUMO']
