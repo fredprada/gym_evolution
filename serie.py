f3aac9e12a94950337c744a2773ec04c02c04617
fredprada <fredprada9@gmail.com>
2023-09-09 14:07:22 -0300

    debugando

--- serie.py
+++ serie.py
@@ -6,22 +6,22 @@ class Serie:
         self.serie_num = serie_num
 
     @st.cache_data
-    def get_series(_self, serie_num):
+    def get_series(_self):
         lista_box_series = []
         for item in range(2, _self.qtd_series + 1):
-            lista_box_series.append(exec(f'series_exercicio_{serie_num}_num_{item} = st.number_input("Séries: ", min_value = 0, max_value = 10, key="serie_{serie_num}_{item}")'))
+            lista_box_series.append(exec(f'series_exercicio_{_self.serie_num}_num_{item} = st.number_input("Séries: ", min_value = 0, max_value = 10, key="serie_{_self.serie_num}_{item}")'))
         return lista_box_series
 
     @st.cache_data
-    def get_reps(_self, serie_num):
+    def get_reps(_self):
         lista_box_reps = []
         for item in range(2, _self.qtd_series + 1):
-            lista_box_reps.append(exec(f'reps_exercicio_{serie_num}_num_{item} = st.number_input("Núm Reps: ", min_value = 0, max_value = 50, key="reps_{serie_num}_{item}")'))
+            lista_box_reps.append(exec(f'reps_exercicio_{_self.serie_num}_num_{item} = st.number_input("Núm Reps: ", min_value = 0, max_value = 50, key="reps_{_self.serie_num}_{item}")'))
         return lista_box_reps
 
     @st.cache_data
-    def get_cargas(_self, serie_num):
+    def get_cargas(_self):
         lista_box_carga = []
         for item in range(2, _self.qtd_series + 1):
-            lista_box_carga.append(exec(f'carga_exercicio_{serie_num}_num_{item} = st.number_input("Carga: ", min_value = 0, max_value = 1000, key="carga_{serie_num}_{item}")'))
+            lista_box_carga.append(exec(f'carga_exercicio_{_self.serie_num}_num_{item} = st.number_input("Carga: ", min_value = 0, max_value = 1000, key="carga_{_self.serie_num}_{item}")'))
         return lista_box_carga
