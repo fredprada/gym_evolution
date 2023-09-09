c022719d22c04a37b96e0d2a1df79b7f2b1dd560
fredprada <fredprada9@gmail.com>
2023-09-08 23:52:50 -0300

    debugando

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ lista_treinos = ['1- PEITO | OMBRO | TRÍCEPS',
                  '3- PERNA']
 lista_marombas = ['Fred','Mari']
 
-col1, col2, col3, col4 = st.columns([2, 1, 1, 1])
+col1, col2, col3, col4 = st.columns([2, 1, 1, 1.3])
 with col1:
     treino_selecionado = st.selectbox('Escolha o treino do dia:', (lista_treinos))
 with col2:
@@ -21,7 +21,7 @@ with col2:
 with col3:
     data = st.date_input('Data', value=hora_atual, format="DD/MM/YYYY",)
 with col4:
-    exercicio_num = st.selectbox("Quantos ex você vai fazer?", ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+    exercicio_num = st.selectbox("Qtd de exercícios", ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
 
 st.markdown("""---""")
 
