54284ab503f11151c5ba0af16b6ae5bc7667f351
fredprada <fredprada9@gmail.com>
2023-09-13 12:16:07 -0300

    debugando

--- main.py
+++ main.py
@@ -29,56 +29,7 @@ with col4:
 st.markdown("""---""")
 
 lista_exercicio = Exercicio(treino_selecionado).get_exercicio()
-lista_variaveis = []
-lista_series_difs = []
-lista_nomes = []
 
-def get_series(qtd_series, serie_num):
-    lista_box_series = []
-    for item in range(2, qtd_series + 1):
-        lista_box_series.append(exec(f'series_exercicio_{serie_num}_num_{item} = st.number_input("Séries: ", min_value = 0, max_value = 10, key="serie_{serie_num}_{item}")'))
-    return lista_box_series
-
-# def get_reps(self):
-#     lista_box_reps = []
-#     for item in range(2, self.qtd_series + 1):
-#         lista_box_reps.append(exec(f'reps_exercicio_{self.serie_num}_num_{item} = st.number_input("Núm Reps: ", min_value = 0, max_value = 50, key="reps_{self.serie_num}_{item}")'))
-#     return lista_box_reps
-
-# def get_cargas(self):
-#     lista_box_carga = []
-#     for item in range(2, self.qtd_series + 1):
-#         lista_box_carga.append(exec(f'carga_exercicio_{self.serie_num}_num_{item} = st.number_input("Carga: ", min_value = 0, max_value = 1000, key="carga_{self.serie_num}_{item}")'))
-#     return lista_box_carga
-lista_var_1 = []
-lista_var_multip = []
-
-# for num in range(1, exercicio_num + 1):
-#     # global exec(f'select_exercicio{num}'),
-#     #         exec(f'qtd_series_exercicio_{num}'),
-#     #         exec(f'series_exercicio_1_{num}'),
-#     #         exec(f'reps_exercicio_1_{num}'),
-#     #         exec(f'carga_exercicio_1_{num}'),
-#     #         exec(f'series_exercicio_{qtd_series}_num_{num}'),
-#     #         exec(f'reps_exercicio_{qtd_series}_num_{num}'),
-#     #         exec(f'carga_exercicio_{qtd_series}_num_{num}')
-#     col1, col2, col3, col4 = st.columns([3,1,1,1])
-#     with col1:
-#         exec(f'select_exercicio{num} = st.selectbox("Exercício ({num}): ", (lista_exercicio))')
-#         exec(f'qtd_series_exercicio_{num} = st.selectbox("Número de séries diferentes:", ([1, 2, 3, 4, 5]), key = "series_dif_{num}")')
-#     with col2:
-#         exec(f'series_exercicio_1_{num} = st.number_input("Séries: ", min_value = 0, max_value = 10, key = "serie_{num}")')
-#         exec(f'Serie(qtd_series_exercicio_{num}, {num}).get_series()')
-#     with col3:
-#         exec(f'reps_exercicio_1_{num} = st.number_input("Núm Reps: ", min_value = 0, max_value = 50, key = "reps_{num}")')
-#         exec(f'Serie(qtd_series_exercicio_{num}, {num}).get_reps()')
-#     with col4:
-#         exec(f'carga_exercicio_1_{num} = st.number_input("Carga: ", min_value = 0, max_value = 10000, key = "carga_{num}")')
-#         exec(f'Serie(qtd_series_exercicio_{num}, {num}).get_cargas()')
-       
-#     exec(f'qtd_series = qtd_series_exercicio_{num}')
-    
-# Crie um dicionário para armazenar os valores
 valores = {}
 
 for num in range(1, exercicio_num + 1):
@@ -86,11 +37,9 @@ for num in range(1, exercicio_num + 1):
     with col1:
         select_exercicio = st.selectbox(f'Exercício ({num}): ', (lista_exercicio))
         qtd_series_exercicio = st.selectbox("Número de séries diferentes:", ([1, 2, 3, 4, 5]), key=f"series_dif_{num}")
-        # Armazene os valores em um dicionário
         valores[f'select_exercicio{num}'] = select_exercicio
         valores[f'qtd_series_exercicio_{num}'] = qtd_series_exercicio
 
-    # Crie listas para armazenar os valores das séries
     series_values = []
     reps_values = []
     carga_values = []
@@ -106,111 +55,16 @@ for num in range(1, exercicio_num + 1):
             carga = st.number_input(f'Carga {serie_num}: ', min_value=0, max_value=10000, key=f'carga_{num}_{serie_num}')
             carga_values.append(carga)
 
-    # Armazene as listas de valores das séries no dicionário
     valores[f'series_exercicio_{num}'] = series_values
     valores[f'reps_exercicio_{num}'] = reps_values
     valores[f'carga_exercicio_{num}'] = carga_values
-
-    # qtd_series = qtd_series_exercicio
-
-    # lista_var_1.append([f'select_exercicio{num}',
-    #                 f'qtd_series_exercicio_{num}',
-    #                 f'series_exercicio_1_{num}',
-    #                 f'reps_exercicio_1_{num}',
-    #                 f'carga_exercicio_1_{num}'])
-    # lista_var_multip.append([f'series_exercicio_{qtd_series}_num_{num}',
-    #                          f'reps_exercicio_{qtd_series}_num_{num}',
-    #                          f'carga_exercicio_{qtd_series}_num_{num}'])
-
-    # for qtd in range(1, qtd_series + 1):
-    #     var_series_dif = [f'series_exercicio_{qtd}_num_{num}',
-    #                       f'reps_exercicio_{qtd}_num_{num}',
-    #                       f'carga_exercicio_{qtd}_num_{num}']
-    #     lista_series_difs.append(var_series_dif)
-    # variaveis_conjunto = [f'select_exercicio{num}',
-    #                       f'qtd_series_exercicio_{num}',
-    #                       f'series_exercicio_1_{num}',
-    #                       f'reps_exercicio_1_{num}',
-    #                       f'carga_exercicio_1_{num}']
-    # lista_variaveis.append(variaveis_conjunto)
     st.markdown("""---""")
-
-# lista_variaveis_completa = lista_variaveis + lista_series_difs
-
-# def flatten_list(nested_list):
-#     return list(itertools.chain(*nested_list))
-
-# flat_lista_variaveis_completa = flatten_list(lista_variaveis_completa)
-
-# lista_dados_coletados = [{'var':lista_variaveis}]
-# dict_info = {}
-# for num in range(1, len(lista_variaveis)):
-#     dict_info[f"exec(f'select_exercicio{num}')"] = exec(f'select_exercicio{num}')
-#     dict_info[f"exec(f'qtd_series_exercicio_{num}')"] = exec(f'qtd_series_exercicio_{num}')
-
-#     dict_info[f"exec(f'series_exercicio_1_{num}')"] = exec(f'series_exercicio_1_{num}')
-#     dict_info[f"exec(f'reps_exercicio_1_{num}')"] = exec(f'reps_exercicio_1_{num}')
-#     dict_info[f"exec(f'carga_exercicio_1_{num}')"] = exec(f'carga_exercicio_1_{num}')
-    
-#     dict_info[f"exec(f'series_exercicio_2_{num}')"] = exec(f'series_exercicio_2_{num}')
-#     dict_info[f"exec(f'reps_exercicio_2_{num}')"] = exec(f'reps_exercicio_2_{num}')
-#     dict_info[f"exec(f'carga_exercicio_2_{num}')"] = exec(f'carga_exercicio_2_{num}')
-    
-#     dict_info[f"exec(f'series_exercicio_3_{num}')"] = exec(f'series_exercicio_3_{num}')
-#     dict_info[f"exec(f'reps_exercicio_3_{num}')"] = exec(f'reps_exercicio_3_{num}')
-#     dict_info[f"exec(f'carga_exercicio_3_{num}')"] = exec(f'carga_exercicio_3_{num}')
-    
-#     dict_info[f"exec(f'series_exercicio_4_{num}')"] = exec(f'series_exercicio_4_{num}')
-#     dict_info[f"exec(f'reps_exercicio_4_{num}')"] = exec(f'reps_exercicio_4_{num}')
-#     dict_info[f"exec(f'carga_exercicio_4_{num}')"] = exec(f'carga_exercicio_4_{num}')
-    
-#     dict_info[f"exec(f'series_exercicio_5_{num}')"] = exec(f'series_exercicio_5_{num}')
-#     dict_info[f"exec(f'reps_exercicio_5_{num}')"] = exec(f'reps_exercicio_5_{num}')
-#     dict_info[f"exec(f'carga_exercicio_5_{num}')"] = exec(f'carga_exercicio_5_{num}')
-#     lista_dados_coletados.append(dict_info)
-
-# lista_var_ajustadas = []
-# lista_qtds=[]
-# for tamanho in range(len(lista_var_multip)):
-#     qtd_series = int(lista_var_multip[tamanho][0][-7])
-#     lista_qtds.append(qtd_series)
-
-# for tamanho in range(len(lista_var_multip)):
-#     for item in range(1, lista_qtds[tamanho] + 1):
-#         lista_var_ajustadas.append([f'series_exercicio_{item}_num_{tamanho + 1}',
-#                                     f'reps_exercicio_{item}_num_{tamanho + 1}',
-#                                     f'carga_exercicio_{item}_num_{tamanho + 1}'])
-
-# lista_nomes_vars = []
-# for item in range(len(lista_var_ajustadas)):
-#     for size in range(len(lista_var_ajustadas[item])):
-#         lista_nomes_vars.append(lista_var_ajustadas[item][size])
-        
-# lista_variaveis_valores = []
-# for item in (lista_nomes_vars):
-#     valor = globals().get(item)
-    # lista_variaveis_valores.append(valor)
-
-# for variavel in lista_var_completa:
-#     variaveis_valores = {}  # Crie um novo dicionário para cada iteração
-#     valor = globals().get(variavel, None)
-#     if isinstance(valor, (int, str)):
-#         variaveis_valores[variavel] = valor
-#     lista_variaveis_valores.append(variaveis_valores)
 
 botao_salvar = st.button('Salvar')
 botao_ver_dados = st.button('Ver tabela com dados do banco')
 
 if botao_salvar:
-    # ETL list_to_add = func_add_row(date_of_the_game,time_played,pai,played_alone,time_of_the_game,enthusiasm_before_playing,rating,listened_to_music,rest_time,feeling_before_game,calorias)
-    salvar_dados_mongodb(valores).database_insertion()
+    salvar_dados_mongodb([valores]).database_insertion()
 
 if botao_ver_dados:
-    # ETL list_to_add = func_add_row(date_of_the_game,time_played,pai,played_alone,time_of_the_game,enthusiasm_before_playing,rating,listened_to_music,rest_time,feeling_before_game,calorias)
-    # salvar_dados_mongodb(lista_dados_coletados).retrieve_data_from_mongodb()
-    # st.sidebar.text(flat_lista_variaveis_completa)
-    # st.sidebar.text(lista_variaveis_completa)
-    # st.sidebar.text(lista_variaveis_valores)
-    # st.sidebar.text(lista_var_1)
-    # st.sidebar.text(lista_var_multip)
     st.sidebar.text(valores)
