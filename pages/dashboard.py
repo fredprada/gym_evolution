bde913069b9fcd4d681e7bf8c823ec8c4cc5c442
fredprada <fredprada9@gmail.com>
2023-09-13 23:01:14 -0300

    Editando dashboard

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -2,7 +2,6 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 from get_dados import get_dados_mongodb
-import openpyxl
 
 ######################################################################################################################################
 # Defining page properties and title, header and subheader
