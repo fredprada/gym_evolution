d6948215f146043fb26b8d5b1adee62b28a552b3
fredprada <fredprada9@gmail.com>
2023-09-20 00:48:23 -0300

    adicionando métricas no dashboard

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -3,6 +3,14 @@ import plotly.express as px
 import pandas as pd
 from get_dados import get_dados_mongodb
 import datetime
+from exercicio import Exercicio
+
+# [OK] Treinos por semana
+# [NOK] Evolução da melhor série do exercício
+# [NOK] Calendário com dias que treinou
+# [NOK] Última carga
+# [NOK] Carga máxima
+# [NOK] Carga média
 
 ######################################################################################################################################
 # Defining page properties and title, header and subheader
@@ -47,19 +55,106 @@ col1.plotly_chart(fig, theme=None, use_container_width=True)
 
 #######################################################################################
 # Evolução da maior carga por exercício
-lista_treinos = df_valores_filtrado['treino'].unique()
-lista_metricas = []
-for treino in lista_treinos:
-    filtrado = df_valores_filtrado[df_valores_filtrado['treino'] == treino].sort_values('data', ascending = False)
-    ex1 = filtrado[['select_exercicio1']]
-    ex1_carga = max(max(filtrado['carga_exercicio_1']))
-    lista_metricas.append([ex1, ex1_carga])
+lista_treinos = ['1- PEITO | OMBRO | TRÍCEPS', 
+                 '2- COSTAS | BÍCEPS',
+                 '3- PERNA']
 
-st.table(lista_metricas)
+treino_selecionado = st.selectbox('Escolha o treino:', (lista_treinos))
+lista_exercicio = Exercicio(treino_selecionado).get_exercicio()
 
-# [OK] Treinos por semana
-# [NOK] Evolução da melhor série do exercício
-# [NOK] Calendário com dias que treinou
-# [NOK] Última carga
-# [NOK] Carga máxima
-# [NOK] Carga média
+df_agupado_por_treino = pd.DataFrame(df_valores_filtrado[df_valores_filtrado['treino'] == treino_selecionado])
+
+def max_valor(lista):
+    return max(lista)
+df_agupado_por_treino['max_carga'] = df_agupado_por_treino['carga_exercicio_1'].apply(max_valor)
+resultado = df_agupado_por_treino.groupby('select_exercicio1')['max_carga'].max().reset_index()
+
+def get_dataframes_from_treino(treino_selecionado):
+    if treino_selecionado == '1- PEITO | OMBRO | TRÍCEPS':
+        ##################################################################################################################
+        # COSTAS E BICEPS
+        df1 = df_agupado_por_treino[df_agupado_por_treino['select_exercicio1'] == 'COSTAS | PUXADA ALTA FRONTAL COM PEGADA ABERTA']
+        df1 = df1[['data','max_carga']]
+        titulo1 = 'Puxada Alta'
+
+        df2 = df_agupado_por_treino[df_agupado_por_treino['select_exercicio1'] == 'COSTAS | REMADA BAIXA']
+        df2 = df2[['data','max_carga']]
+        titulo2 = 'Remada Baixa'
+
+        df3 = df_agupado_por_treino[df_agupado_por_treino['select_exercicio1'] == 'COSTAS | PULL DOWN']
+        df3 = df3[['data','max_carga']]
+        titulo3 = 'Pull Down'
+        
+        df4 = df_agupado_por_treino[df_agupado_por_treino['select_exercicio1'] == 'BÍCEPS | ROSCA DIRETA']
+        df4 = df4[['data','max_carga']]
+        titulo4 = 'Rosca Direta'
+
+        df5 = df_agupado_por_treino[df_agupado_por_treino['select_exercicio1'] == 'BÍCEPS | ROSCA MARTELO']
+        df5 = df5[['data','max_carga']]
+        titulo5 = 'Rosca Martelo'
+
+    elif treino_selecionado == '2- COSTAS | BÍCEPS':
+        ##################################################################################################################
+        # PEITO, OMBRO E TRICEPS
+        df1 = df_agupado_por_treino[df_agupado_por_treino['select_exercicio1'] == 'PEITO | SUPINO RETO']
+        df1 = df1[['data','max_carga']]
+        titulo1 = 'Supino Reto'
+
+        df2 = df_agupado_por_treino[df_agupado_por_treino['select_exercicio1'] == 'OMBRO | ELEVAÇÃO LATERAL']
+        df2 = df2[['data','max_carga']]
+        titulo2 = 'Elevação Lateral'
+        
+        df3 = df_agupado_por_treino[df_agupado_por_treino['select_exercicio1'] == 'OMBRO | DESENVOLVIMENTO COM HALTER']
+        df3 = df3[['data','max_carga']]
+        titulo3 = 'Desenvolvimento'
+        
+        df4 = df_agupado_por_treino[df_agupado_por_treino['select_exercicio1'] == 'OMBRO | REMADA ALTA']
+        df4 = df4[['data','max_carga']]
+        titulo4 = 'Remada Alta'
+        
+        df5 = df_agupado_por_treino[df_agupado_por_treino['select_exercicio1'] == 'TRÍCEPS | TRÍCEPS CORDA']
+        df5 = df5[['data','max_carga']]
+        titulo5 = 'Tríceps Corda'
+        
+    elif treino_selecionado == '3- PERNA':
+        ##################################################################################################################
+        # PERNA
+        df1 = df_agupado_por_treino[df_agupado_por_treino['select_exercicio1'] == 'PERNA | AGACHAMENTO']
+        df1 = df1[['data','max_carga']]
+        titulo1 = 'Agachamento'
+        
+        df2 = df_agupado_por_treino[df_agupado_por_treino['select_exercicio1'] == 'PERNA | EXTENSORA']
+        df2 = df2[['data','max_carga']]
+        titulo2 = 'Extensora'
+        
+        df3 = df_agupado_por_treino[df_agupado_por_treino['select_exercicio1'] == 'PERNA | FLEXORA']
+        df3 = df3[['data','max_carga']]
+        titulo3 = 'Flexora'
+        
+        df4 = df_agupado_por_treino[df_agupado_por_treino['select_exercicio1'] == 'PERNA | PANTURRILHA SENTADO']
+        df4 = df4[['data','max_carga']]
+        titulo4 = 'Panturrilha'
+        
+        df5 = df_agupado_por_treino[df_agupado_por_treino['select_exercicio1'] == 'PERNA | LEVANTAMENTO TERRA SUMO']
+        df5 = df5[['data','max_carga']]
+        titulo5 = 'Terra'
+
+    return df1, df2, df3, df4, df5, titulo1, titulo2, titulo3, titulo4, titulo5
+
+df1, df2, df3, df4, df5, titulo1, titulo2, titulo3, titulo4, titulo5 = get_dataframes_from_treino(treino_selecionado)
+    
+col1, col2, col3 = st.columns([1, 1, 1])
+fig = px.line(df1, 
+              x='data', 
+              y='max_carga',
+              text='max_carga',
+              markers=True, 
+              width=600, 
+              height=300)
+fig.update_traces(textposition='top center', 
+                  textfont_size=16)
+fig.update_layout(yaxis_title=None,
+                  xaxis_title=None,
+                  yaxis={'visible': False})
+fig.update_layout(title=dict(text=titulo1, font=dict(size=22)))
+col1.plotly_chart(fig, theme=None, use_container_width=True)
